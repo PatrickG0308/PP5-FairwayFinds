e2ed8b7827a959dd2a8f1fdf249f462bd8bb6c0b
Patrick Grant <grant.paddy@gmail.com>
2024-03-14 11:24:14 +0000

    Added contact\enquiry form

---
+++ contact/admin.py
@@ -0,0 +1,16 @@
+"""imports for admin page"""
+
+from django.contrib import admin
+from contact.models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    """Allows admin to manage enquiries via the admin panel"""
+    list_display = (
+        'name',
+        'email',
+        'date',
+    )
+
+
+admin.site.register(Contact, ContactAdmin)
