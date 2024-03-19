e6ba5461f03ca8015911e8c7fcd6e83330882a7f
Patrick Grant <grant.paddy@gmail.com>
2024-03-19 16:13:06 +0000

    Removed webmanifest from favicon settings

--- fairwayfinds/settings.py
+++ fairwayfinds/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ["fairwayfinds-6ee6966230e5.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["fairwayfinds-6ee6966230e5.herokuapp.com", "8000-patrickg030-pp5fairwayf-s07u5xfggfc.ws-eu110.gitpod.io"]
 
 
 # Application definition
