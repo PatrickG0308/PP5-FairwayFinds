72c5c8bb553fdb9912769a083f199ec840cb5645
Patrick Grant <grant.paddy@gmail.com>
2024-03-19 15:18:17 +0000

    Deployment to heroku

--- fairwayfinds/settings.py
+++ fairwayfinds/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 if os.path.exists("env.py"):
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ['8000-patrickg030-pp5fairwayf-s07u5xfggfc.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['fairwayfinds-6ee6966230e5.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -130,10 +131,15 @@ WSGI_APPLICATION = 'fairwayfinds.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
