1efc0b4215b1f506894656b1ddab6a3adc374e18
Patrick Grant <grant.paddy@gmail.com>
2024-03-20 20:50:54 +0000

    Added extra product, changed image names, changed DB to point at Elephant SQL

--- fairwayfinds/settings.py
+++ fairwayfinds/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ["fairwayfinds-6ee6966230e5.herokuapp.com", "8000-patrickg030-pp5fairwayf-s07u5xfggfc.ws-eu110.gitpod.io"]
+ALLOWED_HOSTS = ["8000-patrickg030-pp5fairwayf-neo4um0reb5.ws-eu110.gitpod.io"]
 
 
 # Application definition
@@ -132,17 +132,18 @@ WSGI_APPLICATION = 'fairwayfinds.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#if 'DATABASE_URL' in os.environ:
-#    DATABASES = {
-#        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#    }
-#else:
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+#else:
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 
 
 # Password validation
@@ -187,30 +188,30 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if 'USE_AWS' in os.environ:
-        # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
+#if 'USE_AWS' in os.environ:
+#        # Cache control
+#    AWS_S3_OBJECT_PARAMETERS = {
+#        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+#        'CacheControl': 'max-age=94608000',
+#    }
     
-    # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'fairwayfinds'
-    AWS_S3_REGION_NAME = 'eu-west-1'
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+#    # Bucket Config
+#    AWS_STORAGE_BUCKET_NAME = 'fairwayfinds'
+#    AWS_S3_REGION_NAME = 'eu-west-1'
+#    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+#    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+#    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
+#    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+#    STATICFILES_LOCATION = 'static'
+#    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+#    MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+#    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+#    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
     
 
 # Default primary key field type
