7c7150924c206a948f072a8441912e5517dc1d9a
Patrick Grant <grant.paddy@gmail.com>
2024-03-20 01:57:07 +0000

    Added imgs to products and about, and added FAQs

--- fairwayfinds/settings.py
+++ fairwayfinds/settings.py
@@ -132,12 +132,12 @@ WSGI_APPLICATION = 'fairwayfinds.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
+#if 'DATABASE_URL' in os.environ:
+#    DATABASES = {
+#        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#    }
+#else:
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
