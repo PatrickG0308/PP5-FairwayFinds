ad25555ce38fe6d72a3acefc1a52547a9b99de38
Patrick Grant <grant.paddy@gmail.com>
2024-04-30 10:22:20 +0000

    Removed commented out code

--- fairwayfinds/settings.py
+++ fairwayfinds/settings.py
@@ -132,13 +132,13 @@ WSGI_APPLICATION = "fairwayfinds.wsgi.application"
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
-# else:
-#    DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.sqlite3',
-#            'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+   }
+}
 
 
 # Password validation
